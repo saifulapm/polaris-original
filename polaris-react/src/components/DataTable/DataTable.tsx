84a0580899ab1922a602299bc3d3eacbfc8e667a
JeremyLudwigDev <jeremy.ludwig@shopify.com>
2022-04-18 10:27:21 -0400

    Extend column beyond top of table

--- polaris-react/src/components/DataTable/DataTable.tsx
+++ polaris-react/src/components/DataTable/DataTable.tsx
@@ -246,6 +246,7 @@ class DataTableInner extends PureComponent<CombinedProps, DataTableState> {
         isScrolledFarthestRight={isScrolledFarthestRight}
         navigateTableLeft={this.navigateTable('left')}
         navigateTableRight={this.navigateTable('right')}
+        ref={this.navigation}
       />
     );
 
