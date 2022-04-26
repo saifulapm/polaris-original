8257623cf207944aceb16a12018ba7d45298e4b7
Romello Goodman <romello.goodman@shopify.com>
2022-04-26 10:59:42 -0400

    testing the new alias importing for the rc

---
+++ polaris.shopify.com/src/pages/tokens-rc.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as polaris_tokens from '@shopify/polaris-tokens';
+import motion from '@shopify/polaris-tokens/json/motion.json';
+
+console.log(polaris_tokens);
+console.log(motion);
+
+const Page = () => {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  )
+}
+
+export default Page;
